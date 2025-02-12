115c695e942ad3b6127ec302ca4f24c7baa4f110
yegor-vin <egorvinvit@gmail.com>
2025-02-12 20:22:52 +0200

    desktop version completed

--- src/app.js
+++ src/app.js
@@ -3,17 +3,17 @@
 const accordion = document.querySelector('.accordion');
 
 accordion.addEventListener('click', function () {
-    this.classList.toggle('active');
+  this.classList.toggle('active');
 
-    const panel = this.nextElementSibling;
+  const panel = this.nextElementSibling;
 
-    if (panel.style.display === 'block' && panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.display = 'block';
-      panel.style.maxHeight = panel.scrollHeight + 'px';
-    }
-  });
+  if (panel.style.display === 'block' && panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.display = 'block';
+    panel.style.maxHeight = panel.scrollHeight + 'px';
+  }
+});
 
 const modal = document.querySelector('.modal');
 
@@ -43,18 +43,47 @@ const panelImage = document.querySelector('.panel__table-img');
 const panelModalImage = document.querySelector('.panel__modal-content');
 const panelCaptionText = document.querySelector('.panel__caption');
 
-panelImage.onclick = function() {
+panelImage.onclick = function () {
   panelModal.style.display = 'block';
   panelModalImage.src = this.src;
   panelCaptionText.innerHTML = this.alt;
 };
 
-const panelModalCloseBtn = document.querySelector('.panel__modal-close')
+const panelModalCloseBtn = document.querySelector('.panel__modal-close');
 
-panelModalCloseBtn.onclick = function() {
-panelModal.style.display = 'none'
+panelModalCloseBtn.onclick = function () {
+  panelModal.style.display = 'none';
+};
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.querySelectorAll('.slideshow__img-container');
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 1700);
 }
 
+const batteriesGallery = document.querySelector('.batteries__image-container');
+
+const mainImg = batteriesGallery.querySelector('.batteries__main-img');
+
+const itemImages = batteriesGallery.querySelectorAll('.slider__item');
+
+batteriesGallery.addEventListener('click', (event) => {
+  event.preventDefault();
+  const parent = event.target.parentNode;
+  const imageLink = parent.getAttribute('href');
+
+  mainImg.src = imageLink;
+});
 
 
 
